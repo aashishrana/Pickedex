dbb417dd94413ed7017434902253da558b17c93c
aashishrana <aashishrana71135@gmail.com>
2023-08-12 11:44:47 -0700

    A new Gaming Project Journey started

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+// import { useState } from 'react'
+import './App.css'
+import Pokedex from './components/Pokedex/Pokedex'
+
+function App() {
+  
+
+  return (
+    <>
+    <h1>Jai Shree Ram</h1>
+    <Pokedex></Pokedex>
+    </>
+  )
+}
+
+export default App
---
+++ src/components/Pokedex/Pokedex.jsx
@@ -0,0 +1,12 @@
+function Pokedex() {
+
+    return (
+        <>
+        <h2>Sita Ram</h2>
+        
+        </>
+    )
+
+}
+
+export default Pokedex;
